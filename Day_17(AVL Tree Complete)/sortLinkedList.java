cddbe77897b7ff162266a49e320d571ce541b12e
bhanu <bhanublez@gmail.com>
2023-08-06 20:24:47 +0530

    Day 17

---
+++ Day_17(AVL Tree Complete)/sortLinkedList.java
@@ -0,0 +1,28 @@
+//https://www.geeksforgeeks.org/sort-a-linked-list-of-0s-1s-or-2s/
+public class sortLinkedList{
+class Solution
+{
+    //Tode cheating ki hai, dyan sai solution ko deko
+    // static Node segregate(Node head)
+    // {
+    //     int[] count = new int[3]; 
+    //     Node current = head;
+
+    //     while (current != null) {
+    //         count[current.data]++;
+    //         current = current.next;
+    //     }
+
+    //     current = head;
+    //     for (int i = 0; i < 3; i++) {
+    //         while (count[i] > 0) {
+    //             current.data = i;
+    //             count[i]--;
+    //             current = current.next;
+    //         }
+    //     }
+
+    //     return head;
+    // }
+    }
+}
